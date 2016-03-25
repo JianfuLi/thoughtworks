7df8e37b14be9548f133fd70f2300fe2e93807e1
JeffLi <jeffli.cn@outlook.com>
2016-03-25 11:59:27 +0800

    [+] 只有一瓶可乐时打印小票

--- tests/CashRegisterTest.php
+++ tests/CashRegisterTest.php
@@ -14,9 +14,13 @@ class CashRegisterTest extends \PHPUnit_Framework_TestCase
 {
     function testPrint()
     {
-        $expected = '';
         $cashRegister = new CashRegister();
-        $actual = $cashRegister->print();
-        $this->assertEquals($expected, $actual);
+        $this->assertEquals(
+            "***<没钱赚商店>购物清单***" . PHP_EOL .
+            "名称：可口可乐，数量：1瓶，单价：3.00(元)，小计：3.00(元)" . PHP_EOL .
+            "----------------------" . PHP_EOL .
+            "总计：3.00(元)" . PHP_EOL .
+            "**********************"
+            , $cashRegister->print("['ITEM000001']"));
     }
 }
