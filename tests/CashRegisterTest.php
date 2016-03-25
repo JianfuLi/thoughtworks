0894ac3e1078fc87362f0e2729eb566304cff81c
JeffLi <jeffli.cn@outlook.com>
2016-03-25 15:06:46 +0800

    [+] 多种商品条码叠加

--- tests/CashRegisterTest.php
+++ tests/CashRegisterTest.php
@@ -78,5 +78,16 @@ class CashRegisterTest extends \PHPUnit_Framework_TestCase
             '**********************'
             , $cashRegister->print('["ITEM000001","ITEM000002","ITEM000003"]')
         );
+
+        $this->assertEquals(
+            '***<没钱赚商店>购物清单***' . PHP_EOL .
+            '名称：可口可乐，数量：2瓶，单价：3.00(元)，小计：6.00(元)' . PHP_EOL .
+            '名称：羽毛球，数量：2个，单价：5.00(元)，小计：10.00(元)' . PHP_EOL .
+            '名称：苹果，数量：1斤，单价：5.50(元)，小计：5.50(元)' . PHP_EOL .
+            '----------------------' . PHP_EOL .
+            '总计：21.50(元)' . PHP_EOL .
+            '**********************'
+            , $cashRegister->print('["ITEM000001","ITEM000001","ITEM000002-2","ITEM000003"]')
+        );
     }
 }
