29d848d856858b4aeed74a1ac65b2d2ae1a3aadf
JeffLi <jeffli.cn@outlook.com>
2016-03-25 14:52:01 +0800

    [+] 打印3种商品的小票

--- tests/CashRegisterTest.php
+++ tests/CashRegisterTest.php
@@ -67,5 +67,16 @@ class CashRegisterTest extends \PHPUnit_Framework_TestCase
             '**********************'
             , $cashRegister->print('["ITEM000001","ITEM000002"]')
         );
+
+        $this->assertEquals(
+            '***<没钱赚商店>购物清单***' . PHP_EOL .
+            '名称：可口可乐，数量：1瓶，单价：3.00(元)，小计：3.00(元)' . PHP_EOL .
+            '名称：羽毛球，数量：1个，单价：5.00(元)，小计：5.00(元)' . PHP_EOL .
+            '名称：苹果，数量：1斤，单价：5.50(元)，小计：5.50(元)' . PHP_EOL .
+            '----------------------' . PHP_EOL .
+            '总计：13.50(元)' . PHP_EOL .
+            '**********************'
+            , $cashRegister->print('["ITEM000001","ITEM000002","ITEM000003"]')
+        );
     }
 }
